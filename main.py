be7abb0cd8e7b24adca6c25bf67207698dd35c8c
keanu2040 <keanu2040@gmail.com>
2025-05-29 23:45:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user(username):
+    # Greet the user
+    return f'Hello {username} !'
+
+# Pass the required route to the decorator.
+@app.route("/hello")
+def hello():
+    return "Hello, Welcome to GeeksForGeeks"
+  
+@app.route("/")
+def index():
+    return "Homepage of GeeksForGeeks"
+
+if __name__ == "__main__":
+    app.run(debug=True)
